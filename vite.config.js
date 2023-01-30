3aa3d7799491e79a439e2c12b774fc4c68d715f0
Tyler Davis <tallyndavis@comcast.net>
2023-01-30 16:13:03 -0500

    fixed typo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-digital-business-site/'
+  base: '/react-digital-business-card/'
 })
